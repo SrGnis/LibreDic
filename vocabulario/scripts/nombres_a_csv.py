f73bcce1d26c5dc102574d480289d4a0a0acb4b8
SrGnis <gnisms97@gmail.com>
2021-01-31 14:49:38 +0100

    nombre a csv completado

--- vocabulario/scripts/nombres_a_csv.py
+++ vocabulario/scripts/nombres_a_csv.py
@@ -2,32 +2,46 @@ import csv
 import sys
 
 head = ["PALABRA", "CATEGORIA", "TIPO",
-        "GENERO", "NUMERO", "GRADO"]
+        "GENERO", "NUMERO"]
 save = []
 
 
 def formate(linea):
-    if linea[0] == "irregularizar":
-        linea.insert(1, "AR")
-        linea.insert(3, "")
-        linea.insert(4, "ACTUAL")
-    elif("NORECONOCIDO" in linea):
-        linea.insert(1, linea[0][-2:].upper())
-        linea.insert(2, "")
-        linea.insert(3, "")
-        linea.insert(4, "")
-    else:
-        linea.append("RECONOCIDO")
-        if("IRREGULAR" in linea and not( "IR" in linea or "AR" in linea or "ER" in linea)):
-            linea.insert(1, linea[0][-2:].upper())
-        if(not ("REFLEXIVO" in linea)):
-            linea.insert(3, "NOREFLEXIVO")
+    l = []
+    l.append(linea[0])
+    l.append("NOMBRE")
 
-    return linea
+    if(linea[1][1] == 'C'):
+        l.append("COMÚN")
+    else:
+        l.append("PROPIO")
+
+    if(len(linea) != 6):
+
+        l.append("")
+        l.append("")
+
+    else:
+
+        if(linea[1][2] == 'M'):
+            l.append("MASCULINO")
+        elif(linea[1][2] == 'F'):
+            l.append("FEMENINO")
+        else:
+            l.append("COMÚN")
+        
+        if(linea[1][3] == 'M'):
+            l.append("SINGULAR")
+        elif(linea[1][3] == 'F'):
+            l.append("PLURAL")
+        else:
+            l.append("INVARIABLE")
+
+    return l
 
 
 def main():
-    f = open("./nombres.txt", "r")
+    f = open("nombres.txt", "r")
     for l in f:
         l = l.split(' ')
         l = formate(l)
