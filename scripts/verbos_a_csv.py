b7847a732311d3a6bdd867e7540a2c013980390e
SrGnis <gnisms97@gmail.com>
2021-02-07 20:02:20 +0100

    "INFINITIVO" cambiado por "PALABRA" en verbos

--- scripts/verbos_a_csv.py
+++ scripts/verbos_a_csv.py
@@ -1,7 +1,7 @@
 import csv
 import sys
 
-head = ["INFINITIVO", "TERMINACIÓN", "REGULAR",
+head = ["PALABRA", "CATEGORÍA", "TERMINACIÓN", "REGULAR",
         "REFLEXIVO", "ACTUAL", "RECONOCIDO"]
 save = []
 
@@ -11,23 +11,26 @@ def formate(linea):
         linea.insert(1, "AR")
         linea.insert(3, "")
         linea.insert(4, "ACTUAL")
+        linea.insert(1, "VERBO")
     elif("NORECONOCIDO" in linea):
         linea.insert(1, linea[0][-2:].upper())
         linea.insert(2, "")
         linea.insert(3, "")
         linea.insert(4, "")
+        linea.insert(1, "VERBO")
     else:
         linea.append("RECONOCIDO")
-        if("IRREGULAR" in linea and not( "IR" in linea or "AR" in linea or "ER" in linea)):
+        if("IRREGULAR" in linea and not("IR" in linea or "AR" in linea or "ER" in linea)):
             linea.insert(1, linea[0][-2:].upper())
         if(not ("REFLEXIVO" in linea)):
             linea.insert(3, "NOREFLEXIVO")
+        linea.insert(1, "VERBO")
 
     return linea
 
 
 def main():
-    f = open("./verbos.txt", "r")
+    f = open("./texto_plano/verbos.txt", "r")
     for l in f:
         l = l.replace(' ', '')
         l = l.replace(']', '')
@@ -38,7 +41,7 @@ def main():
         save.append(l)
     f.close()
 
-    with open('verbos.csv', 'w', newline='') as csvfile:
+    with open('./csv/verbos.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(head)
         for r in save:
