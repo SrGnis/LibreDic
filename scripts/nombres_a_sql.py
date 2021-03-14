ed82dcd85c1a7406f0fe43578494dfe5f26643c5
SrGnis <gnisms97@gmail.com>
2021-03-14 22:32:21 +0100

    actualización

--- scripts/nombres_a_sql.py
+++ scripts/nombres_a_sql.py
@@ -20,7 +20,21 @@ with open('./csv/nombres.csv', newline='') as csvfile:
         else:
             numeros.append(row[4])
 
+lemas = list(dict.fromkeys(lemas))#eliminar duplicados
+
+'''
 print("INSERT INTO PALABRAS (lema, raiz) VALUES")
-for x in lemas:
-    print("(\"{lema}\",NULL),".format(lema=x))
+for p in lemas:
+    if '_' not in p:
+        print("(\"{lema}\",NULL),".format(lema=p))
 print(';')
+'''
+
+'''
+print("INSERT INTO CARACTERISTICAS (id_palabra, id_categoria) VALUES")
+for p in lemas:
+    if '_' not in p:
+        print("((SELECT (id_palabra) FROM PALABRAS WHERE lema=\"{lema}\"),(SELECT (id_tipo_categoria) FROM TIPOS_CATEGORIAS WHERE tipo_categoria=\"SUS\")),".format(lema=p))
+print(';')
+'''
+
