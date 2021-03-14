da3b60a59856e0e63f42a7e57e0bafbc1ec54a3d
SrGnis <gnisms97@gmail.com>
2021-03-14 22:32:03 +0100

    Creado script de tipos de propiedades

---
+++ sql/tipos_propiedades.sql
@@ -0,0 +1,13 @@
+INSERT INTO TIPOS_PROPIEDADES (tipo_propiedad) VALUES
+("tipo"),
+("genero"),
+("número"),
+("persona"),
+("caso"),
+("función"),
+("número_poseedor"),
+("terminación"),
+("regular"),
+("reflexivo"),
+("actual"),
+("reconocido");
