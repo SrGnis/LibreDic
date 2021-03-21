eff45c556a46d19e1f77b479c41d6691790c4c7a
SrGnis <gnisms97@gmail.com>
2021-03-21 18:15:35 +0100

    adverb script created

--- scripts/adv2csv.py
+++ scripts/adv2csv.py
@@ -3,30 +3,23 @@
 import csv
 import sys
 
-head = ["PALABRA", "CATEGORÍA", "TIPO"]
+head = ["WORD", "ROOT", "CATEGORY", "TYPE"]
 save = []
 
 
-def formate(linea):
-    print(linea)
-    l = []
-    l.append(linea[0])
-    l.append("ADVERVIO")
-    l.append("GENERAL")
-
-    return l
-
-
 def main():
-    f = open("./texto_plano/advervios.txt", "r")
+    f = open("data/src/adv.txt", "r")
     for l in f:
         if(l != ""):
+            l = l.replace('\n', '')
             l = l.split(' ')
-            l = formate(l)
+            p = list(l[2])
+            del l[2]
+            l.extend(p)
             save.append(l)
     f.close()
 
-    with open('./csv/advervios.csv', 'w') as csvfile:
+    with open('data/csv/adv.csv', 'w') as csvfile:
         writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
         writer.writerow(head)
         for r in save:
