aa4ece6cbf2c5fd85c59c86bb1a8a55b9f6610a7
SrGnis <gnisms97@gmail.com>
2021-03-21 18:17:06 +0100

    noun script created

---
+++ scripts/sus2csv.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+import csv
+import sys
+
+head = ["WORD", "ROOT", "CATEGORY", "TYPE",
+"GENERE", "NUMBER", "NECLASS", "NESUBCLASS", "DEGREE"]
+save = []
+
+
+def main():
+    f = open("data/src/sus.txt", "r")
+    for l in f:
+        if(l != ""):
+            l = l.replace('\n', '')
+            l = l.split(' ')
+            p = list(l[2])
+            del l[2]
+            l.extend(p)
+            save.append(l)
+    f.close()
+
+    with open('data/csv/sus.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(head)
+        for r in save:
+            writer.writerow(r)
+
+
+if __name__ == "__main__":
+    main()
