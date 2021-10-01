c5f69baa8b8103d6ad56a07351979ed6d09c89a3
Genís Martínez <49316776+SrGnis@users.noreply.github.com>
2021-10-01 11:34:43 +0200

    Nueva estructura de la bse de datos
    La estructura sigue el esta organizacion:
    Cada palabra puede tener varios significados, estos significados dotan a la plalabra de diferentes caracteristicas (categoria y propiedades).

--- sql/structure.sql
+++ sql/structure.sql
@@ -1,57 +1,57 @@
 CREATE TABLE `PALABRAS` (
-  `id_palabra` int(11) NOT NULL AUTO_INCREMENT,
-  `lema` varchar(35) NOT NULL,
-  `raiz` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id_palabra`),
-  UNIQUE KEY `PALABRAS_UN` (`lema`),
-  KEY `PALABRAS_RAIZ` (`raiz`),
-  CONSTRAINT `PALABRAS_RAIZ` FOREIGN KEY (`raiz`) REFERENCES `PALABRAS` (`id_palabra`) ON DELETE SET NULL ON UPDATE CASCADE
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `TIPOS_CATEGORIAS` (
-  `id_tipo_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `tipo_categoria` char(1) NOT NULL,
-  PRIMARY KEY (`id_tipo_categoria`),
-  UNIQUE KEY `TIPOS_CATEGORIAS_UN` (`tipo_categoria`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `CATEGORIAS` (
-  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `id_palabra` int(11) NOT NULL,
-  `id_tipo_categoria` int(11) NOT NULL,
-  PRIMARY KEY (`id_categoria`),
-  UNIQUE KEY `CATEGORIAS_UN` (`id_palabra`,`id_categoria`),
-  KEY `CATEGORIAS_PALABRA` (`id_palabra`),
-  KEY `CATEGORIAS_TIPO` (`id_tipo_categoria`),
-  CONSTRAINT `CATEGORIAS_PALABRA` FOREIGN KEY (`id_palabra`) REFERENCES `PALABRAS` (`id_palabra`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `CATEGORIAS_TIPO` FOREIGN KEY (`id_tipo_categoria`) REFERENCES `TIPOS_CATEGORIAS` (`id_tipo_categoria`) ON UPDATE CASCADE
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `SENTIDOS` (
-  `id_sentido` int(11) NOT NULL AUTO_INCREMENT,
-  `id_categoria` int(11) NOT NULL,
-  PRIMARY KEY (`id_sentido`),
-  KEY `SENTIDOS_FK` (`id_categoria`),
-  CONSTRAINT `SENTIDOS_FK` FOREIGN KEY (`id_categoria`) REFERENCES `CATEGORIAS` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `TIPOS_PROPIEDADES` (
-  `id_tipo_propiedad` int(11) NOT NULL AUTO_INCREMENT,
-  `tipo_propiedad` varchar(30) NOT NULL,
-  PRIMARY KEY (`id_tipo_propiedad`),
-  UNIQUE KEY `TIPOS_PROPIEDADES_UN` (`tipo_propiedad`)
-) DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-CREATE TABLE `PROPIEDADES` (
-  `id_propiedad` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tipo_propiedad` int(11) NOT NULL,
-  `valor` varchar(30) COLLATE utf8mb4_bin NOT NULL,
-  `id_sentido` int(11) NOT NULL,
-  PRIMARY KEY (`id_propiedad`),
-  KEY `PROPIEDADES_TIPO` (`id_tipo_propiedad`),
-  KEY `PROPIEDADES_SENTIDO` (`id_sentido`),
-  CONSTRAINT `PROPIEDADES_SENTIDO` FOREIGN KEY (`id_sentido`) REFERENCES `SENTIDOS` (`id_sentido`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `PROPIEDADES_TIPO` FOREIGN KEY (`id_tipo_propiedad`) REFERENCES `TIPOS_PROPIEDADES` (`id_tipo_propiedad`) ON UPDATE CASCADE
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `lema` varchar(35) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `PALABRAS_UN` (`lema`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-COMMIT;
+CREATE TABLE `CATEGORIAS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_categoria` char(1) COLLATE utf8mb4_bin NOT NULL,
+  `tipo_categoria_entero` varchar(35) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `TIPOS_PROPIEDADES` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo_propiedad` char(1) COLLATE utf8mb4_bin NOT NULL,
+  `tipo_propiedad_entero` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `TIPOS_PROPIEDADES_UN` (`tipo_propiedad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `SENTIDOS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_palabra` int(11) NOT NULL,
+  `id_raiz` int(11) DEFAULT NULL,
+  `sentido` text COLLATE utf8mb4_bin,
+  PRIMARY KEY (`id`),
+  KEY `SENTIDOS_FK` (`id_palabra`),
+  KEY `SENTIDOS_FK_1` (`id_raiz`),
+  CONSTRAINT `SENTIDOS_FK` FOREIGN KEY (`id_palabra`) REFERENCES `PALABRAS` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `SENTIDOS_FK_1` FOREIGN KEY (`id_raiz`) REFERENCES `PALABRAS` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `CARACTERISTICAS` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sentido` int(11) NOT NULL,
+  `id_categoria` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `CARACTERISTICAS_FK` (`id_sentido`),
+  KEY `CARACTERISTICAS_FK_1` (`id_categoria`),
+  CONSTRAINT `CARACTERISTICAS_FK` FOREIGN KEY (`id_sentido`) REFERENCES `SENTIDOS` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `CARACTERISTICAS_FK_1` FOREIGN KEY (`id_categoria`) REFERENCES `CATEGORIAS` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `PROPIEDADES` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_tipo_propiedad` int(11) NOT NULL,
+  `valor` varchar(30) COLLATE utf8mb4_bin NOT NULL,
+  `id_caracteristicas` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `PROPIEDADES_TIPO` (`id_tipo_propiedad`),
+  KEY `PROPIEDADES_SENTIDO` (`id_caracteristicas`),
+  CONSTRAINT `PROPIEDADES_SENTIDO` FOREIGN KEY (`id_caracteristicas`) REFERENCES `CARACTERISTICAS` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `PROPIEDADES_TIPO` FOREIGN KEY (`id_tipo_propiedad`) REFERENCES `TIPOS_PROPIEDADES` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
