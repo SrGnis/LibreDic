f9d7674fd1e1d21656529e8f10276e26d2025b00
SrGnis <gnisms97@gmail.com>
2022-05-05 14:35:39 +0200

    Actualizadas las querys a la nueva estructura

--- sql/querys/general.sql
+++ sql/querys/general.sql
@@ -1,5 +1,5 @@
 #Información de una palabra
-SELECT pal.lema, s.id AS id_sentido, cat.tipo_categoria AS categoria, tp.tipo_propiedad AS propiedad, pro.valor 
+SELECT pal.lema, s.id AS id_sentido, car.categoria, pro.propiedad, pro.valor 
 FROM
 (
     SELECT *
@@ -8,12 +8,10 @@ FROM
 ) AS pal
 JOIN sentidos s ON s.id_palabra = pal.id
 JOIN caracteristicas car ON car.id_sentido = s.id
-JOIN categorias cat ON car.id_categoria = cat.id
-JOIN propiedades pro ON car.id = pro.id_caracteristicas 
-JOIN tipos_propiedades tp ON pro.id_tipo_propiedad = tp.id
+JOIN propiedades pro ON car.id = pro.id_caracteristicas;
 
 #Información de una palabra verbose
-SELECT pal.lema, s.id AS id_sentido, cat.tipo_categoria AS categoria, tp.tipo_propiedad AS propiedad, pro.valor, cat.tipo_categoria_entero AS categoria_v, tp.tipo_propiedad_entero AS tipo_v, pvv.verbose AS valor_v 
+SELECT pal.lema, s.id AS id_sentido, car.categoria, pro.propiedad, pro.valor, cv.verbose AS categoria_v, pv.verbose AS propiedad_v, pvv.verbose AS valor_v
 FROM
 (
     SELECT *
@@ -22,85 +20,71 @@ FROM
 ) AS pal
 JOIN sentidos s ON s.id_palabra = pal.id
 JOIN caracteristicas car ON car.id_sentido = s.id
-JOIN categorias cat ON car.id_categoria = cat.id
-JOIN propiedades pro ON car.id = pro.id_caracteristicas 
-JOIN tipos_propiedades tp ON pro.id_tipo_propiedad = tp.id
-LEFT JOIN propiedad_valor_verbose pvv ON pro.valor = pvv.valor AND tp.tipo_propiedad = pvv.tipo AND cat.tipo_categoria = pvv.categoria;
+JOIN categorias_verbose cv ON car.categoria = cv.categoria 
+JOIN propiedades pro ON car.id = pro.id_caracteristicas
+JOIN propiedades_verbose pv ON pv.propiedad = pro.propiedad 
+LEFT JOIN propiedad_valor_verbose pvv ON pro.valor = pvv.valor AND pro.propiedad = pvv.tipo AND car.categoria = pvv.categoria;
 
 #Todas la palabras con su categoria
 SELECT 
   tabla.palabra,
   tabla.raiz,
-  tabla.tipo_categoria AS categoria
+  tabla.categoria
 FROM (
-  SELECT c.id, p.lema AS palabra, r.lema AS raiz, c2.tipo_categoria 
+  SELECT c.id, p.lema AS palabra, r.lema AS raiz, c.categoria 
   FROM sentidos s 
   JOIN palabras p ON p.id = s.id_palabra
   JOIN palabras r ON r.id = s.id_raiz
   JOIN caracteristicas c ON c.id_sentido = s.id
-  JOIN categorias c2 ON c2.id = c.id_categoria
 ) tabla;
 
 #Solo las palabras de cierta categoria Ej. solo verbos
 SELECT p.id, p.lema 
-FROM caracteristicas car 
-JOIN (SELECT * FROM categorias WHERE tipo_categoria = 'V') AS cat
-	ON car.id_categoria = cat.id
+FROM (SELECT * FROM caracteristicas WHERE categoria = "V") car
 JOIN sentidos s ON s.id = car.id_sentido 
 JOIN palabras p ON p.id = s.id_palabra;
 
 #Solo las palabras con una cierta propiedad Ej. solo palabras masculinas
-SELECT pal.lema, cat.tipo_categoria
+SELECT pal.lema, car.categoria 
 FROM
 (
     SELECT pro.*
     FROM propiedades pro
-    JOIN (SELECT * FROM tipos_propiedades tp WHERE tipo_propiedad = 'G') AS tp
-        ON pro.id_tipo_propiedad = tp.id
-    WHERE valor = 'M'
+    WHERE propiedad = 'G' AND valor = 'M'
 ) AS p
 JOIN caracteristicas car ON p.id_caracteristicas = car.id
-JOIN categorias cat ON car.id_categoria = cat.id 
 JOIN sentidos s ON s.id = car.id_sentido 
 JOIN palabras pal ON pal.id = s.id_palabra;
 
 #Solo las palabras con CIERTAS propiedades Ej. solo palabras masculinas en singular
-SELECT pal.lema, cat.tipo_categoria
+SELECT pal.lema, car.categoria
 FROM caracteristicas car
 JOIN
 (
     SELECT pro.*
     FROM propiedades pro
-    JOIN (SELECT * FROM tipos_propiedades tp WHERE tipo_propiedad = 'G') AS tp
-        ON pro.id_tipo_propiedad = tp.id
-    WHERE valor = 'M'
+    WHERE propiedad = 'G' AND valor = 'M'
 ) AS p1
 ON car.id = p1.id_caracteristicas
 JOIN 
 (
     SELECT pro.*
     FROM propiedades pro
-    JOIN (SELECT * FROM tipos_propiedades tp WHERE tipo_propiedad = 'N') AS tp
-        ON pro.id_tipo_propiedad = tp.id
-    WHERE valor = 'S'
+    WHERE propiedad = 'N' AND valor = 'S'
 ) AS p2
 ON car.id = p2.id_caracteristicas
-JOIN categorias cat ON car.id_categoria = cat.id 
 JOIN sentidos s ON s.id = car.id_sentido 
 JOIN palabras pal ON pal.id = s.id_palabra;
 
 #Solo las palabras con una cierta propiedad y caracteristica Ej. solo adjetivos masculinos
-SELECT pal.lema, cat.tipo_categoria
+SELECT pal.lema, car.categoria
 FROM
 (
     SELECT pro.*
     FROM propiedades pro
-    JOIN (SELECT * FROM tipos_propiedades tp WHERE tipo_propiedad = 'G') AS tp
-        ON pro.id_tipo_propiedad = tp.id
-    WHERE valor = 'M'
+    WHERE propiedad = 'G' AND valor = 'M'
 ) AS p
-JOIN caracteristicas car ON p.id_caracteristicas = car.id
-JOIN (SELECT * FROM categorias WHERE tipo_categoria = 'A') cat ON car.id_categoria = cat.id
+JOIN (SELECT * FROM caracteristicas WHERE categoria = "A") car ON p.id_caracteristicas = car.id
 JOIN sentidos s ON s.id = car.id_sentido 
 JOIN palabras pal ON pal.id = s.id_palabra;
 
@@ -109,24 +93,24 @@ JOIN palabras pal ON pal.id = s.id_palabra;
 SELECT 
   tabla.palabra,
   tabla.raiz,
-  tabla.tipo_categoria AS categoria,
+  tabla.categoria,
   t.valor AS tipo,
   g.valor AS genero,
   n.valor AS numero
 FROM (
-  SELECT c.id, p.lema AS palabra, r.lema AS raiz, c2.tipo_categoria 
+  SELECT c.id, p.lema AS palabra, r.lema AS raiz, c.categoria 
   FROM sentidos s 
   JOIN palabras p ON p.id = s.id_palabra
   JOIN palabras r ON r.id = s.id_raiz
   JOIN caracteristicas c ON c.id_sentido = s.id
-  JOIN categorias c2 ON c2.id = c.id_categoria
 ) tabla
 INNER JOIN propiedades t
   on tabla.id = t.id_caracteristicas
-  and t.id_tipo_propiedad = 1
+  and t.propiedad = 'T'
 INNER JOIN propiedades g
   on tabla.id = g.id_caracteristicas
-  and g.id_tipo_propiedad = 3
+  and g.propiedad = 'G'
 INNER JOIN propiedades n
   on tabla.id = n.id_caracteristicas
-  and n.id_tipo_propiedad = 4
+  and n.propiedad = 'N';
+ 
