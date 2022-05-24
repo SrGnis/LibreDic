695fd0797e6df4875c713132af872d867833d87e
srgnis <gnisms97@gmail.com>
2022-05-24 20:54:10 +0200

    Added deletion of files in the sentidos script

--- scripts/sentidos/parse_wiktionary.sh
+++ scripts/sentidos/parse_wiktionary.sh
@@ -1,10 +1,15 @@
+wget https://dumps.wikimedia.org/eswiktionary/20220520/eswiktionary-20220520-pages-meta-current.xml.bz2
+tar -xf eswiktionary-20220520-pages-meta-current.xml.bz2
+rm eswiktionary-20220520-pages-meta-current.xml.bz2
 cat eswiktionary-20220520-pages-meta-current.xml | xq . > eswiktionary.json
+rm eswiktionary-20220520-pages-meta-current.xml
 grep -n -m 1 "\"page\":" eswiktionary.json |sed  's/\([0-9]*\).*/\1/'
 sed -i 1,148d eswiktionary.json
 sed -i "$(( $(wc -l <eswiktionary.json)-3+1 )),$ d" eswiktionary.json
 sed -i '1i [' eswiktionary.json
 sed -i -e '$a\ ]' eswiktionary.json
 mongoimport --db eswiktionary --collection palabras --type json --file eswiktionary.json --jsonArray
+rm eswiktionary.json
 
 #En mongosh
 use eswiktionary
