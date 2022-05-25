7a1343da28c2a7887af4d91cdc889e7a22f07e63
gmrtnz <genis.martinez@biotechvana.com>
2022-05-25 14:55:17 +0200

    some fixes to the script

--- scripts/sentidos/parse_wiktionary.sh
+++ scripts/sentidos/parse_wiktionary.sh
@@ -1,5 +1,7 @@
+## needs python library xq and package jq
+
 wget https://dumps.wikimedia.org/eswiktionary/20220520/eswiktionary-20220520-pages-meta-current.xml.bz2
-tar -xf eswiktionary-20220520-pages-meta-current.xml.bz2
+bzip2 -dk eswiktionary-20220520-pages-meta-current.xml.bz2
 rm eswiktionary-20220520-pages-meta-current.xml.bz2
 cat eswiktionary-20220520-pages-meta-current.xml | xq . > eswiktionary.json
 rm eswiktionary-20220520-pages-meta-current.xml
@@ -12,7 +14,7 @@ mongoimport --db eswiktionary --collection palabras --type json --file eswiktion
 rm eswiktionary.json
 
 #En mongosh
-use eswiktionary
+db = db.getSiblingDB("eswiktionary")
 db.palabras.deleteMany({title: {$regex : ":"}})
 db.palabras.aggregate( [ { $project : { _id: "$title", title : 1, text: "$revision.text.#text" } },{ $out : "out" } ] )
 
